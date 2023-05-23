d53ef7d109ef931c4879e999fbb0478b04ec9f83
Andrey Bogoslovskiy <88778576+bojieitsit@users.noreply.github.com>
2023-05-23 17:35:51 +0400

    Initial Commit

---
+++ GifTestArcaneGaming/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GifTestArcaneGaming
+//
+//  Created by Andrey on 23.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
